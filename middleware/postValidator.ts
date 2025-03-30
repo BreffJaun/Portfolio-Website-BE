390a5759c3b863f14db38637492aef6086896786
BreffJaun <braun_jeff@web.de>
2025-03-30 15:19:33 +0200

    Refactored the post validator

--- middleware/postValidator.ts
+++ middleware/postValidator.ts
@@ -3,30 +3,17 @@ import { body } from "express-validator";
 
 // C R E A T E   V A L I D A T O R
 export const postValidator = [
-  body("authorId")
-    .notEmpty()
-    .withMessage("Author ID is required!")
-    .trim()
-    .escape(),
+  body("authorId").notEmpty().withMessage("Author ID is required!").trim(),
   body("authorName").trim().escape(),
   body("authorAvatar").trim().escape(),
   body("authorAction").trim().escape(),
-  body("vibe")
-    .optional()
-    // .withMessage("Vibe is optional! A short text with an Emoji")
-    .trim()
-    .escape(),
+  body("vibe").optional().trim(),
   body("articleTitle").optional().trim().escape(),
   body("articleContent")
     .notEmpty()
     .withMessage("Article content has to be set!")
-    .trim()
-    .escape(),
-  body("articleImageSrc")
-    .optional()
-    // .isURL()
-    // .withMessage("Article image must be a valid URL!")
     .trim(),
+  body("articleImageSrc").optional().trim(),
   body("articleLink")
     .optional() // Feld ist optional
     .custom((value) => {
@@ -39,39 +26,22 @@ export const postValidator = [
 
 // Validator für Updates
 export const postUpdateValidator = [
-  body("authorId")
-    .notEmpty()
-    .withMessage("Author ID is required!")
-    .trim()
-    .escape(),
+  body("authorId").notEmpty().withMessage("Author ID is required!").trim(),
   body("authorName").trim().escape(),
   body("avatar").trim().escape(),
   body("authorAction").trim().escape(),
-  // body("date")
-  //   .notEmpty()
-  //   .withMessage("Current date is set automatically!")
-  //   .trim(),
-  body("vibe")
-    .optional()
-    // .withMessage("Vibe is optional! A short text with an Emoji")
-    .trim()
-    .escape(),
+  body("vibe").optional().trim(),
   body("articleTitle").optional().trim().escape(),
   body("articleContent")
     .notEmpty()
     .withMessage("Article content has to be set!")
-    .trim()
-    .escape(),
+    .trim(),
   body("articleImageSrc")
     .optional()
     .isURL()
     .withMessage("Article image must be a valid URL!")
     .trim(),
-  // body("articleLink")
-  //   .optional()
-  //   .isURL()
-  //   .withMessage("Article link must be a valid URL!")
-  //   .trim(),
+
   body("articleLink")
     .optional() // Feld ist optional
     .isURL()
